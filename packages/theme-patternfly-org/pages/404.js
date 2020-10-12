5055cbc5dea1c77c34e5e5bde3608dfccb3f7fd0
evwilkin <evan.wilkinson@gmail.com>
2020-10-12 11:26:40 -0400

    started 404 changes

--- packages/theme-patternfly-org/pages/404.js
+++ packages/theme-patternfly-org/pages/404.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'theme-patternfly-org/components';
-import { PageSection, Title } from '@patternfly/react-core';
+import { Button, PageSection, TextInput, Title } from '@patternfly/react-core';
+import SearchIcon from '@patternfly/react-icons/dist/esm/icons/search-icon';
+import TimesIcon from '@patternfly/react-icons/dist/esm/icons/times-icon';
 
 const Page404 = () => (
   <PageSection className="ws-section">
     <Title size="xl" className="pf-u-mb-lg ws-page-title" headingLevel="h1">
-      404 - Page not found
+      404 Error
     </Title>
+    <p>We can't find the page you're looking for. Try searching patternfly.org or go home.</p>
+    <TextInput id="global-search-input"  placeholder="Search" />
+    <SearchIcon className="global-search-icon" />
+    <Button aria-label="Expand search input" variant="plain" className="ws-collapse-search" onClick={() => setSearchExpanded(false)}>
+      <TimesIcon />
+    </Button>
     <ul>
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="/">Return to home page</Link></li>
     </ul>
   </PageSection>
 );
